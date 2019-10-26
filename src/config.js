c6520f14ee797f09b2458bc9ed4df3db82a93c21
aldibatyr <aldibatyrbekov@gmail.com>
2019-10-25 19:43:03 -0700

    changed api endpoint one more time

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 require('dotenv');
 
 export default {
-  API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8000/api',
+  API_ENDPOINT: process.env.API_ENDPOINT || 'https://immense-thicket-82721.herokuapp.com/api',
   TOKEN_KEY: process.env.TOKEN_KEY || 'waitline-app-auth-token'
 }
